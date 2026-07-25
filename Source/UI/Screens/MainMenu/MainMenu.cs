fa6664a3f8f94e92a865152fbdc1a3c7c75c51c1
sajmon <116080178+6ajmon@users.noreply.github.com>
2026-07-25 23:20:52 +0200

    Add credits and exit button

--- Source/UI/Screens/MainMenu/MainMenu.cs
+++ Source/UI/Screens/MainMenu/MainMenu.cs
@@ -5,14 +5,27 @@ public partial class MainMenu : Control
 {
 	
 	[Export] private Button _playButton;
+	[Export] private Button _creditsButton;
+	[Export] private Button _exitButton;
 	[Export] private Camera3D _camera;
 	[Export] private float _cameraSpeed;
 	private bool _cameraGoingUp = true;
+	private ConfirmationDialog _exitDialog;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		_playButton.Pressed += OnPlayButton_Pressed;
+		_playButton.Pressed += OnPlayButtonPressed;
+		_creditsButton.Pressed += OnCreditsButtonPressed;
+		_exitButton.Pressed += OnExitButtonPressed;
+		
+		_exitDialog = new ConfirmationDialog();
+		_exitDialog.Title = "Exit Game";
+		_exitDialog.DialogText = "Are you sure you want to exit the game?";
+		_exitDialog.GetOkButton().Text = "Yes";
+		_exitDialog.GetCancelButton().Text = "No";
+		_exitDialog.Confirmed += () => GetTree().Quit();
+		AddChild(_exitDialog);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,9 +38,19 @@ public partial class MainMenu : Control
 		
 	}
 	
-	private void OnPlayButton_Pressed()
+	private void OnPlayButtonPressed()
 	{
 		GameManager.Instance.EmitSignal(nameof(GameManager.PlayButtonPressed));
+	}
+	
+	private void OnCreditsButtonPressed()
+	{
+		OS.ShellOpen("https://github.com/6ajmon/gmtk2026");
+	}
+	
+	private void OnExitButtonPressed()
+	{
+		_exitDialog.PopupCentered();
 	}
 
 	private void CameraMovement(double delta)
